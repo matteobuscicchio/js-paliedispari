732affe6af8d64c95f5af3ad800361e09f478d11
matteo <matteoezio@me.com>
2020-11-20 15:33:10 +0100

    cartelle di base

---
+++ palindroma/assets/js/main.js
@@ -0,0 +1,7 @@
+// Palidroma = anna | otto | oro | dvd | osso | area | ossesso | ottetto |
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
