3b6fa634231c4c49e09141d85f07ab03db8a2744
matteo <matteoezio@me.com>
2020-11-20 19:32:37 +0100

    Verifica parole o frasi palindrome.
    Ho svolto solo la parte relativa alla verifica della parola palindroma. Durante le ricerche ho scoperto dell'esistenza del RegExp, e dei modificatori annessi perciò ho provato ad usarlo.

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,7 +1,35 @@
-// Palidroma = anna | otto | oro | dvd | osso | area | ossesso | ottetto |
+// Parole o frasi palidrome = anna | otto | oro | dvd | osso | area | ossesso | ottetto | i topi non avevano nipoti |
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+var userWord = prompt('inserisci una parolan o una frase per verificare se è palindroma');
+
+/**
+ * 
+ * @ {*} 
+ */
+function palindromeChecker(word) {
+    //ho scoperto l'esistenza delle RegExp che ti permettono di controllare la sintassi
+    //ho sfruttato la cosa per selezionare ed eliminare gli spazi dalle frasi
+    //così da poterle rendere delle maxi-parole.
+    //ho letto amche dei modificatori i-m-g. 
+    // i - non distingue tra maiuscole e minuscole;
+    // m - esegue ricerche multiline (il pattern viene confrontato con ogni linea del testo);
+    // g - esegue ricerche globali, cioè non si ferma alla prima occorrenza;
+    var removeSpace = / /g;
+    wordSpaceless = word.toLowerCase().replace(removeSpace, '');
+
+    checkerWord = wordSpaceless;
+    reverseCheckerWord = checkerWord.split('').reverse().join(''); 
+    return reverseCheckerWord === wordSpaceless;
+}
+
+if (palindromeChecker(userWord)) {
+    console.log('la parola o la frase è palindroma');
+} else {
+    console.log('la parola o la frase NON è palindroma');
+}
