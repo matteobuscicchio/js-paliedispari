39316c7e9b393dcfa3b1adfe34a44edbb004145d
matteo <matteoezio@me.com>
2020-11-22 18:19:15 +0100

    ho inserito i due cicli di controllo dentro una funzione
    non ho dichiarato gli argomenti

--- pari e dispari/assets/js/main.js
+++ pari e dispari/assets/js/main.js
@@ -9,40 +9,99 @@
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 
-var userLuck = prompt('digita "P" per scegliere pari o "D" per scegliere dispari');
-userLuck = userLuck.toLowerCase();
 
-i=0;
-errore=1;
-while (i < errore) {
-    if (userLuck === "p") {
+
+
+// SVOLGIMENTO
+
+/**
+ * FUNZIONE P/D E NUMERO FORTUNATO
+ * 
+ * 
+ * questa funzione regola e verifica la scelta tra: 
+ * 
+ * 1) pari e dispari;
+ * 2) numero fortunato compreso tra 1 e 5;
+ * 
+ */
+function userChoise() {
+
+    // pari e dispari
+    var userLuck = prompt('digita "P" per scegliere pari o "D" per scegliere dispari');
+    userLuck = userLuck.toLowerCase();
+
+    i=0;
+    errore=1;
+
+    while (i < errore) {
+        if (userLuck === "p") {
         i++;
-    } else if (userLuck === "d") {
+        } else if (userLuck === "d") {
         i++;
-    } else {
+        } else {
         var userLuck = prompt('Hai inserito un valore non ammesso; per favore digita "P" per scegliere pari o "D" per scegliere dispari');
         userLuck = userLuck.toLowerCase();
+        }
     }
-}
+    console.log(userLuck);
 
-userNumber = Number(prompt('inserisci un numero da 1 a 5'));
+    // numero fortunato
+    userNumber = Number(prompt('inserisci un numero da 1 a 5'));
 
+    index=0;
+    numeroSbagliato=1;
 
-index=0;
-numeroSbagliato=1;
-while (index < numeroSbagliato) {
-    if (userNumber === 0) {
-        var userNumber = Number(prompt('Hai inserito un valore non ammesso; scegli un numero da 1 a 5'));
-    } else if (userNumber === 1 || userNumber === 2 || userNumber === 3 || userNumber === 4 || userNumber === 5) {
+    while (index < numeroSbagliato) {
+        if (userNumber === 1 || userNumber === 2 || userNumber === 3 || userNumber === 4 || userNumber === 5) { //ho usato quest asoluzione solo perchè sapevo di avere 5 numeri.
         index++;
-    } else {
+        } else {
         var userNumber = Number(prompt('Hai inserito un valore non ammesso; scegli un numero da 1 a 5'));
+        }
     }
+    console.log(userNumber);
 }
+
+//HO DECISO DI INSERIRE ENTRAMBI I CICLI IN UNA FUNZIONE SENZA DICHIARARNE L'ARGOMENTO
+// var userLuck = prompt('digita "P" per scegliere pari o "D" per scegliere dispari');
+// userLuck = userLuck.toLowerCase();
+
+// i=0;
+// errore=1;
+// while (i < errore) {
+//     if (userLuck === "p") {
+//         i++;
+//     } else if (userLuck === "d") {
+//         i++;
+//     } else {
+//         var userLuck = prompt('Hai inserito un valore non ammesso; per favore digita "P" per scegliere pari o "D" per scegliere dispari');
+//         userLuck = userLuck.toLowerCase();
+//     }
+// }
+
+// userNumber = Number(prompt('inserisci un numero da 1 a 5'));
+
+
+// index=0;
+// numeroSbagliato=1;
+// while (index < numeroSbagliato) {
+//     if (userNumber === 0) {
+//         var userNumber = Number(prompt('Hai inserito un valore non ammesso; scegli un numero da 1 a 5'));
+//     } else if (userNumber === 1 || userNumber === 2 || userNumber === 3 || userNumber === 4 || userNumber === 5) {
+//         index++;
+//     } else {
+//         var userNumber = Number(prompt('Hai inserito un valore non ammesso; scegli un numero da 1 a 5'));
+//     }
+// }
+
+
 
 // function computerNumber() {
 //     computerRandomNumber = Math.floor(Math.random() * 5) + 1;
 // }
 
-console.log(userLuck, userNumber);
+
+
+// ESECUZIONE GIOCO
+
+userChoise();
 
